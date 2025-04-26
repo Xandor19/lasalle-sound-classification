8b2896e6bad1938a3fd471eb0214c0002d7f572e
xandor19 <tomeale19@gmail.com>
2025-04-26 18:27:41 +0200

    feat(ml): implement feature calculator and selector for ML Models
    - feature_extraction defines all possible features and allows subset selection and aggregation specification
    - feature_selection defines approaches to extract a subset of the features

---
+++ steps/machine_learning/feature_extraction.py
@@ -0,0 +1,232 @@
+import numpy as np
+import pandas as pd
+import pywt
+import librosa
+from globals.defaults import *
+from scipy.signal import hilbert
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+# Maps each feature to the transform it requires
+FEATURE_TRANSFORM_MAP = {
+    "mfcc": "fft",
+    "spectral_centroid": "fft",
+    "rolloff": "fft",
+    "flatness": "fft",
+    "bandwidth": "fft",
+    "spectral_flux": "fft",
+    "energy": "wavelet",
+    "entropy": "wavelet",
+    "envelope": "hilbert",
+    "inst_freq": "hilbert",
+    "spectral_flux_env": "hilbert",
+}
+
+
+def aggregate_feature(feat_name, values, methods):
+    """
+    Performs aggregations on the feature values based on the specified methods.
+
+    ## Params:
+    - feat_name: Name of the feature to be aggregated.
+    - values: Values of the feature.
+    - methods: List of aggregation methods to be applied. If "*" is present, all methods are applied.
+
+    ## Returns:
+    - Dictionary with the aggregated values for the feature, one per key, indicating the method used (single
+      element dictionary if no aggregation is needed).
+    """
+    if methods and len(values) > 1:
+        # Aggregated feature
+        if "*" in methods:
+            methods = ["mean", "std", "min", "max", "median", "iqr"]
+
+        result = {}
+
+        for m in methods:
+            sufix = f"_{m}"
+
+            match m:
+                case "mean":
+                    val = np.nanmean(values)
+                case "std":
+                    val = np.nanstd(values)
+                case "iqr":
+                    q75, q25 = np.nanpercentile(values, [75, 25])
+                    val = q75 - q25
+                case "median":
+                    val = np.nanmedian(values)
+                case "min":
+                    val = np.nanmin(values)
+                case "max":
+                    val = np.nanmax(values)
+                case _:
+                    val = values
+                    sufix = ""
+
+            # Register the feature name with the corresponding aggregation
+            result[f"{feat_name}{sufix}"] = val
+    else:
+        # Single value features are directly returned
+        result = {feat_name: values[0] if hasattr(values, '__getitem__') else values}
+
+    return result
+
+
+class FeatureExtractor(BaseEstimator, TransformerMixin):
+    """
+    Feature factory that computes both time and frequency domain features from audio signals. Transform-specific parameters
+    are provided
+
+    ## Params:
+    - feature_config: Dictionary with the features to compute as keys and a list of aggregation methods to apply to them as values. 
+                      It can be None to compute all possible features and aggregations or contain an "aggregator" key to apply
+                      the same aggregation methods to all features, or specify a subset with a "features" key containing just a list of names
+    - fs: Sampling rate of the audio signal.
+    - n_fft: Size of the STFT window.
+    - hop_length: Number of samples between successive frames.
+    - n_mfcc: Number of MFCC coefficients to compute.
+    - wavelet: Type of wavelet to use for wavelet transform.
+    - wavelet_level: Level of the wavelet transform.
+    """
+    def __init__(self, feature_config=None, fs=SAMPLING_RATE, n_fft=FFT_SIZE, hop_length=HOP_LENGTH,
+                 n_mfcc=13, wavelet=WAVELET_TYPE, wavelet_level=WAVELET_LEVEL):
+        # Maps each feature to the function that computes it
+        self._feature_functions = {
+            "zcr": self._compute_zcr,
+            "rms": self._compute_rms,
+            "mfcc": self._compute_mfcc,
+            "spectral_centroid": self._compute_spectral_centroid,
+            "rolloff": self._compute_rolloff,
+            "flatness": self._compute_flatness,
+            "bandwidth": self._compute_bandwidth,
+            "spectral_flux_env": self._compute_spectral_flux_env,
+            "energy": self._compute_energy,
+            "entropy": self._compute_entropy,
+            "envelope": self._compute_envelope,
+            "inst_freq": self._compute_inst_freq,
+            "spectral_flux": self._compute_spectral_flux
+        }
+        # Construct the final feature config to use in the computation
+        if "aggregator" in feature_config.keys():
+            features = feature_config["features"] if "features" in feature_config.keys() else self._feature_functions.keys()
+            self.feature_config = { f: feature_config["aggregator"] for f in features}
+        else:
+            self.feature_config = feature_config if feature_config else { k: ["*"] for k in self._feature_functions.keys() }
+
+        self.fs = fs
+        self.n_fft = n_fft
+        self.hop_length = hop_length
+        self.n_mfcc = n_mfcc
+        self.wavelet = wavelet
+        self.wavelet_level = wavelet_level
+
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        results = []
+        transforms_needed = { FEATURE_TRANSFORM_MAP[feature] for feature in self.feature_config.keys() if feature in FEATURE_TRANSFORM_MAP.keys() }
+
+        for i, x in enumerate(X):
+            feats = {}
+            # Cache all transforms needed to compute the set of features
+            cache = self._compute_transforms(x, transforms_needed)
+
+            for feature_name, aggregations in self.feature_config.items():
+                # Compute the raw value of the corresponding feature
+                raw_values, nested = self._feature_functions[feature_name](x, cache)
+
+                if nested:
+                    # Features that generate multi-dimensional shapes (like MFCC) are aggregated one by one
+                    for i, val in enumerate(raw_values):
+                        feats.update(aggregate_feature(feature_name + f"_{i}", val, aggregations))
+                else:
+                    feats.update(aggregate_feature(feature_name, raw_values, aggregations))
+
+            results.append(feats)
+
+        return pd.DataFrame(results)
+
+    def _compute_transforms(self, x, needed):
+        cache = {}
+        x = np.nan_to_num(x, nan=0.0, posinf=0.0, neginf=0.0)
+
+        if "fft" in needed:
+            cache["fft"] = np.abs(librosa.stft(x, n_fft=self.n_fft, hop_length=self.hop_length))
+
+        if "wavelet" in needed:
+            cache["wavelet"] = pywt.wavedec(x, self.wavelet, level=self.wavelet_level)
+
+        if "hilbert" in needed:
+            analytic = hilbert(x)
+            phase = np.unwrap(np.angle(analytic))
+            inst_freq = np.diff(phase) * self.fs / (2 * np.pi)
+            cache["hilbert"] = {
+                "analytic": analytic,
+                "envelope": np.abs(analytic),
+                "inst_freq": inst_freq
+            }
+
+        return cache
+
+    """
+    Feature computation methods
+    """
+
+    def _compute_zcr(self, x, cache):
+        return ((x[:-1] * x[1:]) < 0).astype(float), False
+
+    def _compute_rms(self, x, cache):
+        return np.array([np.sqrt(np.nanmean(x**2))]), False
+
+    def _compute_mfcc(self, x, cache):
+        x = np.nan_to_num(x, nan=0.0, posinf=0.0, neginf=0.0)
+        mfcc = librosa.feature.mfcc(y=x, sr=self.fs, n_mfcc=self.n_mfcc)
+
+        return mfcc, True
+
+    def _compute_spectral_centroid(self, x, cache):
+        return librosa.feature.spectral_centroid(S=cache["fft"], sr=self.fs)[0], False
+
+    def _compute_rolloff(self, x, cache):
+        return librosa.feature.spectral_rolloff(S=cache["fft"], sr=self.fs)[0], False
+
+    def _compute_flatness(self, x, cache):
+        return librosa.feature.spectral_flatness(S=cache["fft"])[0], False
+
+    def _compute_bandwidth(self, x, cache):
+        return librosa.feature.spectral_bandwidth(S=cache["fft"], sr=self.fs)[0], False
+    
+    def _compute_spectral_flux(self, x, cache):
+        S = cache["fft"]
+        diff = np.diff(S, axis=1)
+        flux = np.nansum(diff**2, axis=0)  # one value per frame
+
+        return flux, False
+
+    def _compute_energy(self, x, cache):
+        return np.array([np.sum(c**2) for c in cache["wavelet"]]), False
+
+    def _compute_entropy(self, x, cache):
+        entropies = []
+
+        for c in cache["wavelet"]:
+            p = c**2 / (np.sum(c**2) + 1e-12)
+            entropies.append(-np.sum(p * np.log2(p + 1e-12)))
+
+        return np.array(entropies), False
+
+    def _compute_envelope(self, x, cache):
+        return cache["hilbert"]["envelope"], False
+
+    def _compute_inst_freq(self, x, cache):
+        return cache["hilbert"]["inst_freq"], False
+    
+    def _compute_spectral_flux_env(self, x, cache):
+        envelope = cache["hilbert"]["envelope"]
+        diff = np.diff(envelope)
+        flux = diff ** 2
+
+        return flux, False
---
+++ steps/machine_learning/feature_selection.py
@@ -0,0 +1,104 @@
+import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import RFE
+from sklearn.feature_selection import SelectKBest, mutual_info_classif
+
+
+class KBestSelector(BaseEstimator, TransformerMixin):
+    """
+    Feature selector based on generalized rankings to select the best features
+
+    ## Params:
+    - method: Method to use for feature selection. Can be an string specifying the method or directly a 
+              callable compatible with Sklearn's SelectKBest. Currently available methods are: "mutual_info" (direct
+              map to sklearn's mutual_info_classif) and "fisher_score" to compute inter-intra class distance.
+    - k: number of features to keep.
+    """
+    def __init__(self, method='fisher_score', k=20):
+        self.method = method
+        self.k = k
+        self.methods = {
+            "mutual_info": mutual_info_classif,
+            "fisher_score": self.fisher_score
+        }
+        if method not in self.methods:
+            raise ValueError(f"Invalid method: {method}. Available methods are: {list(self.methods.keys())}")
+        self.selector = None
+        self.selected_columns_ = None
+
+    def fit(self, X, y):
+        nas = X.isna().sum()
+        print(nas[nas > 0])
+        self.column_names_ = X.columns
+        self.selector = SelectKBest(score_func=self.method if isinstance(self.method, callable) else self.methods[self.method] , k=self.k)
+        self.selector.fit(X, y)
+        mask = self.selector.get_support()
+        self.selected_columns_ = list(X.columns[mask])
+
+        return self
+
+    def transform(self, X):
+        return X[self.selected_columns_]
+
+    def fisher_score(self, X, y):
+        if not isinstance(X, np.ndarray):
+            X = X.values  # ensure numpy array
+
+        classes = np.unique(y)
+        scores = []
+
+        for i in range(X.shape[1]):
+            inter_class = np.var([X[y == c, i].mean() for c in classes])
+            intra_class = np.mean([X[y == c, i].var() for c in classes])
+            score = inter_class / (intra_class + 1e-6)
+            scores.append(score)
+
+        return np.array(scores)
+
+
+class ModelBasedSelector(BaseEstimator, TransformerMixin):
+    """
+    Feature selector based on a model's results with the features
+
+    ## Params:
+    - method: Method to use for feature selection. Can be "rfe" for recursive feature elimination or "rf_importance" for
+              random forest feature importance. If "rfe", a model instance must be provided in the kwargs with name "model".
+              The same model type that would be used on inference should be provided
+    - k: number of features to keep.
+    - kwargs: 
+    """
+    def __init__(self, method='rf_importance', k=20, model=None):
+        self.method = method
+        self.k = k
+        self.model = model
+        self.selector = None
+        self.selected_columns_ = None
+
+    def fit(self, X, y):
+        if self.method == 'rfe':
+            if not model:
+                raise ValueError("Model must be provided for RFE method.")
+
+            self.selector = RFE(model, n_features_to_select=self.k)
+            self.selector.fit(X, y)
+            mask = self.selector.get_support()
+            self.selected_columns_ = list(X.columns[mask])
+
+        elif self.method == 'rf_importance':
+            model = RandomForestClassifier()
+            model.fit(X, y)
+            importances = model.feature_importances_
+            indices = np.argsort(importances)[::-1][:self.k]
+            self.selected_columns_ = [self.column_names_[i] for i in indices]
+
+        return self
+
+    def transform(self, X):
+        return X[self.selected_columns_]
+
+
+catalog = {
+    "kbest": KBestSelector,
+    "modelbased": ModelBasedSelector
+}
