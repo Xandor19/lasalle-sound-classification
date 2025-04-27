72f882c679de4f36942cc1d31a8260a212f843b6
xandor19 <tomeale19@gmail.com>
2025-04-27 19:54:41 +0200

    ref(steps): replace peak normalizer with standardization

--- steps/normalizer.py
+++ steps/normalizer.py
@@ -8,7 +8,7 @@ class RMSNormalizer(BaseEstimator, TransformerMixin):
     
     ## Params:
     - ref_rms: Reference RMS value to normalize the signals to. Defaults to 0.1."""
-    def __init__(self, ref_rms=0.1, **kwargs):
+    def __init__(self, ref_rms=1, **kwargs):
         self.ref_rms = ref_rms
 
     def fit(self, X, y=None):
@@ -22,30 +22,29 @@ class RMSNormalizer(BaseEstimator, TransformerMixin):
             normalized.append(x * (self.ref_rms / rms) if rms > 0 else x)
 
         return normalized
+    
 
-
-class PeakNormalizer(BaseEstimator, TransformerMixin):
+class StandardNormalizer(BaseEstimator, TransformerMixin):
     """
-    Normalizes the signal using the higher peak value that it contains.
+    Normalizes the amplitudes of the signal so they follow standard distribution
     """
     def __init__(self, **kwargs):
         # Placeholder constructor to support generic fixers calls
         pass
 
     def fit(self, X, y=None):
+        concatenated = np.concatenate(X)
+
+        # Compute training mean and std
+        self.train_mean = np.nanmean(concatenated)
+        self.train_std = np.nanstd(concatenated)
         return self
 
     def transform(self, X):
-        normalized = []
-
-        for x in X:
-            max_val = np.max(np.abs(x))
-            normalized.append(x / max_val if max_val > 0 else x)
-
-        return normalized
+        return [(x - self.train_mean) / self.train_std for x in X]
 
 
 catalog = {
     "rms": RMSNormalizer,
-    "peak": PeakNormalizer
+    "standard": StandardNormalizer
 }
