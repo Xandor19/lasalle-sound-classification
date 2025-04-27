fd6dadbbe473a2d0b0f169257aecdc93cacdcd39
xandor19 <tomeale19@gmail.com>
2025-04-27 14:42:54 +0200

    ref(steps): adapt pipelines to use approach (ML or DL) based model instantiation and code improvements
    - remove data extraction from main pipeline, leaving it only as a pipeline definition
    - adapt function naming to convention
    - replace with_fallback logic with built-in dictionary get method

--- pipelines.py
+++ pipelines.py
@@ -1,8 +1,8 @@
 from globals.constants import *
-from steps.input import tagged_audios
 from steps.step_factory import step_factory
-from steps.machine_learning.feature_extraction import FeatureExtractor
 from sklearn.pipeline import Pipeline
+from steps.machine_learning.models import model_factory as ml_factory
+from steps.machine_learning.feature_extraction import FeatureExtractor
 
 
 def multi_model_ranking(config):
@@ -11,23 +11,19 @@ def multi_model_ranking(config):
     for experiment_config in config['experiments']:
         model_pipeline = ml_pipeline(config) if config['model-type'] == 'ml' else dl_pipeline(config)
         
-        experiments[config['experiment-name']] = run_pipeline(model_pipeline, experiment_config)
+        experiments[config['experiment-name']] = PIPELINE(model_pipeline, experiment_config)
 
 
-def run_pipeline(model_pipeline, config):
-    X, y = tagged_audios(config)
-    fixer = step_factory(FIXER, with_fallback(config, FIXER))
-    detector_params = with_fallback(config, FIXER, PARAMS, False)
+def PIPELINE(model_pipeline, config):
+    fixer = step_factory(FIXER, config.get(FIXER))
+    detector_params = config.get(FIXER, {}).get(PARAMS, {})
 
-    pipeline = Pipeline(steps=[
-        (DETECTOR, step_factory(DETECTOR, with_fallback(config, DETECTOR, NAME), instantiate=False)(fixer=fixer, **detector_params)),
-        (DENOISER, step_factory(DENOISER, with_fallback(config, DENOISER))),
-        (NORMALIZER, step_factory(NORMALIZER, with_fallback(config, NORMALIZER))),
+    return Pipeline(steps=[
+        (DETECTOR, step_factory(DETECTOR, config.get(DETECTOR, {}).get(NAME), instantiate=False)(fixer=fixer, **detector_params)),
+        (DENOISER, step_factory(DENOISER, config.get(DENOISER))),
+        (NORMALIZER, step_factory(NORMALIZER, config.get(NORMALIZER))),
         (MODEL, model_pipeline)
     ])
-
-    pipeline.fit(X, y)
-    pipeline.predict(X)
 
 
 def dl_pipeline(config):
@@ -35,23 +31,11 @@ def dl_pipeline(config):
 
 
 def ml_pipeline(config):
-    model = step_factory(MODEL, config[MODEL])
-    selector_params = with_fallback(config, FEATURE_SELECTOR, PARAMS, False)
+    model = ml_factory(config[MODEL])
+    selector_params = config.get(FEATURE_SELECTOR, {}).get(PARAMS, {})
 
     return Pipeline(steps=[
-        (FEATURE_EXTRACTOR, FeatureExtractor(with_fallback(config, FEATURE_EXTRACTOR))),
-        (FEATURE_SELECTOR, step_factory(FEATURE_SELECTOR, with_fallback(config, FEATURE_SELECTOR, NAME), instantiate=False)(model=model, **selector_params)),
+        (FEATURE_EXTRACTOR, FeatureExtractor(config.get(FEATURE_EXTRACTOR, None))),
+        (FEATURE_SELECTOR, step_factory(FEATURE_SELECTOR, config.get(FEATURE_SELECTOR, {}).get(NAME), instantiate=False)(model=model, **selector_params)),
         (MODEL, model)
     ])
-
-
-def with_fallback(conf, key, inner=None, void_return=True):
-    if inner:
-        result = conf[key][inner] if key in conf.keys() and inner in conf[key].keys() else {}
-    else:
-        result = conf[key] if key in conf.keys() else {}
-
-    if not result and void_return:
-        return None
-    else:
-        return result
