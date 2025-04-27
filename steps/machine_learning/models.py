cd97a22fa619ff8ce87d9c57e4be356bd91b225c
xandor19 <tomeale19@gmail.com>
2025-04-27 18:13:03 +0200

    ref(ml): adapt model_factory to naming standard for nested configurations
    - include constants for new use cases

--- steps/machine_learning/models.py
+++ steps/machine_learning/models.py
@@ -4,26 +4,27 @@ from sklearn.ensemble import AdaBoostClassifier, VotingClassifier
 
 
 def model_factory(model_setup):
-    match model_setup[NAME]:
-        case "ada":
-            kwargs = model_setup["config"]
-            base_model = kwargs.get("model", "knn")
-            ensemble_params = kwargs.get(PARAMS, {})
-            base_params = kwargs.get("model_params", {})
+    if model_setup[NAME] == COMPOSITE:
+        kwargs = model_setup[CONFIG]
 
-            return AdaBoostClassifier(base_estimator=catalog[base_model](**base_params), **ensemble_params)
-        case "voting":
-            kwargs = model_setup["config"]
-            base_models = kwargs.get("models", [
-                {NAME: "knn", PARAMS: {}},
-                {NAME: "svm", PARAMS: {}}
-            ])
-            ensemble_params = kwargs.get(PARAMS, {})
-            estimators = [(model[NAME], catalog[model[NAME]](**model.get(PARAMS, {}))) for model in base_models]
+        match model_setup["method"]:
+            case "ada":
+                base_model = kwargs.get("model", "knn")
+                ensemble_params = kwargs.get(PARAMS, {})
+                base_params = kwargs.get("model_params", {})
 
-            return VotingClassifier(estimators=estimators, **ensemble_params)
-        case _:
-            return catalog[model_setup[NAME]](**model_setup.get(PARAMS, {}))
+                return AdaBoostClassifier(base_estimator=catalog[base_model](**base_params), **ensemble_params)
+            case "voting":
+                base_models = kwargs.get("models", [
+                    {NAME: "KNeighborsClassifier", PARAMS: {}},
+                    {NAME: "SVC", PARAMS: {}}
+                ])
+                ensemble_params = kwargs.get(PARAMS, {})
+                estimators = [(model[NAME], catalog[model[NAME]](**model.get(PARAMS, {}))) for model in base_models]
+
+                return VotingClassifier(estimators=estimators, **ensemble_params)
+    else:
+        return catalog[model_setup[NAME]](**model_setup.get(PARAMS, {}))
 
 
 catalog = { e[0]: e[1] for e in all_estimators(type_filter='classifier') }
