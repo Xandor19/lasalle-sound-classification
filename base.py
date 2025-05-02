f669e585960e6ca3f6b2d0339ed914f868866dbc
xandor19 <tomeale19@gmail.com>
2025-05-02 23:39:10 +0200

    ref(steps): fix handling of nan values in transforms
    - subclasses can now access the nan values indices for the current input element
    - add a flag to indicate if the inputs should be remasked after transforms if previously contained nan masks

--- base.py
+++ base.py
@@ -7,8 +7,10 @@ class BaseCustom(BaseEstimator):
     Template class for all custom components that handles the update of parameters and the application
     of batch transformations
     """
-    def __init__(self, na_tolerant=True):
+    def __init__(self, na_tolerant=True, rebuild_transformed=True):
         self.na_tolerant = na_tolerant
+        self.rebuild_transformed = rebuild_transformed
+        self.na_indices = None
 
     def transform(self, X):
         transformed = []
@@ -16,18 +18,19 @@ class BaseCustom(BaseEstimator):
         for x in X:
             if not self.na_tolerant:
                 # Safeguard against possible NaN values from previous steps when the method is not na-tolerant
-                na_indices = np.isnan(x)
+                self.na_indices = np.isnan(x)
 
-                if np.any(na_indices): 
+                if np.any(self.na_indices): 
                     x = np.nan_to_num(x, **self._na_fill(x))
 
-            new_waveform = self._apply_transform(x)
+            output = self._apply_transform(x)
 
-            if not self.na_tolerant and np.any(na_indices):
+            if not self.na_tolerant and self.rebuild_transformed and np.any(self.na_indices):
                 # Reset na indices to their original value
-                new_waveform[na_indices] = np.nan
+                output[self.na_indices] = np.nan
 
-            transformed.append(new_waveform)
+            self.na_indices = None
+            transformed.append(output)
 
         return transformed
 
